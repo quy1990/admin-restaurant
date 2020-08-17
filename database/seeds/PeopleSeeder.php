5bc970d0784c38e3c7ceefa6939c8d4e9974b382
Nguyen Tu Quy <tu.nguyen@unitedprint.com>
2020-08-18 00:28:05 +0200

    generate Data

---
+++ database/seeds/PeopleSeeder.php
@@ -0,0 +1,95 @@
+<?php
+
+use App\Models\Invitation;
+use App\Models\People;
+use App\Models\Reservation;
+use App\Models\Restaurant;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class PeopleSeeder extends Seeder
+{
+    private $quantity = 10;
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        People::truncate();
+        User::truncate();
+        Restaurant::truncate();
+        Reservation::truncate();
+        Invitation::truncate();
+
+        $this->generateRealData();
+    }
+
+    protected function generateObjects()
+    {
+        factory(User::class, $this->quantity)->create();
+
+        factory(Restaurant::class, $this->quantity)->create();
+
+        for ($i = 0; $i < $this->quantity; $i++) {
+            factory(Reservation::class)->create([
+                'user_id'       => random_int(1, $this->quantity),
+                'restaurant_id' => random_int(1, $this->quantity)
+            ]);
+        }
+
+        for ($i = 0; $i < $this->quantity; $i++) {
+            factory(Invitation::class)->create([
+                'user_id'        => random_int(1, $this->quantity),
+                'reservation_id' => random_int(1, $this->quantity),
+                'restaurant_id'  => random_int(1, $this->quantity),
+            ]);
+        }
+
+        for ($i = 0; $i < $this->quantity; $i++) {
+            factory(People::class)->create([
+                'reservation_id' => random_int(1, $this->quantity),
+                'invitation_id'  => random_int(1, $this->quantity),
+                'restaurant_id'  => random_int(1, $this->quantity),
+                'user_id'        => random_int(1, $this->quantity),
+            ]);
+        }
+
+    }
+
+    public function generateRealData()
+    {
+        for ($i = 0; $i < $this->quantity; $i++) {
+            $user = factory(User::class)->create();
+
+            $restaurant = factory(Restaurant::class)->create();
+
+            for ($i = 0; $i < 5; $i++) {
+                $reservation = factory(Reservation::class)->create([
+                    'user_id'       => $user->id,
+                    'restaurant_id' => $restaurant->id
+                ]);
+
+                for ($i = 0; $i < 5; $i++) {
+                    $invitation = factory(Invitation::class)->create([
+                        'user_id'        => $user->id,
+                        'restaurant_id'  => $restaurant->id,
+                        'reservation_id' => $reservation
+                    ]);
+
+                    for ($i = 0; $i < 5; $i++) {
+                        factory(People::class)->create([
+                            'user_id'        => $user->id,
+                            'restaurant_id'  => $restaurant->id,
+                            'reservation_id' => $reservation,
+                            'invitation_id'  => $invitation,
+                        ]);
+                    }
+                }
+            }
+        }
+
+    }
+}
