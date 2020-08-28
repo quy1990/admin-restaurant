6d775c73efeeaf8f8d29879916a2f96db7f8ca07
Nguyen Tu Quy <tu.nguyen@unitedprint.com>
2020-08-28 13:48:02 +0200

    fixed move to create/ Folder

---
+++ app/Events/Created/CreateAReservationEvent.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events\Created;
+
+use App\Models\Reservation;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CreateAReservationEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $reservation;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     *
+     * @param Reservation $reservation
+     */
+    public function __construct(Reservation $reservation)
+    {
+        $this->reservation = $reservation;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ app/Events/Created/CreateAnInvitationEvent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events\Created;
+use App\Models\Invitation;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CreateAnInvitationEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $invitation;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     *
+     * @param Invitation $invitation
+     */
+    public function __construct(Invitation $invitation)
+    {
+        $this->invitation = $invitation;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
